bc5e7767113b23309335d475efa15e18c71318d6
klezworm <klezworm@gmail.com>
2021-04-23 14:43:50 +0200

    logo orvea

--- index.php
+++ index.php
@@ -1,4 +1,11 @@
 <?php ?>
+<html>
+    <head>
+  <link rel="shortcut icon" type="image/x-icon" href="img/orvealogo.png" />
+</head>
+<body>
 <li><a href="Simuler le budjet.html">Simuler le budjet !</a></li>
 <li><a href="Télécharger la plaquette commerciale.html">Télécharger la plaquette commerciale !</a></li>
 
+</body>
+</html>
