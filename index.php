e83cae78937a544b3eac8e98a9717cfa35b367db
klezworm <klezworm@gmail.com>
2021-04-23 14:59:22 +0200

    logo orvea

--- index.php
+++ index.php
@@ -2,10 +2,12 @@
 <html>
     <head>
   <link rel="shortcut icon" type="image/x-icon" href="img/orvealogo.png" />
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
 </head>
 <body>
+    <ul class="list-group">
 <li><a href="Simuler le budjet.html">Simuler le budjet !</a></li>
 <li><a href="Télécharger la plaquette commerciale.html">Télécharger la plaquette commerciale !</a></li>
-
+</ul>
 </body>
 </html>
