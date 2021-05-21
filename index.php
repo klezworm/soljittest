c302c1312831d54f4a90d45f0bf2927a18d9e79c
klezworm <klezworm@gmail.com>
2021-05-21 10:55:16 +0200

    Télécharger la plaquette commerciale(prod)

--- index.php
+++ index.php
@@ -14,6 +14,7 @@
 <li><a href="6.Newsletters.html">Newsletters.</a></li>
 <li><a href="7.invisible reCAPTCHA.html">7.invisible reCAPTCHA.</a></li>
 <li><a href="8.Simuler le budget (prod).html">8.Simuler le budget (prod).</a></li>
+<li><a href="9.Télécharger la plaquette commerciale(prod).html">9.Simuler le budget (prod).</a></li>
 </ul>
 </body>
 </html>
